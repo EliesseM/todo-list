7d02e67d8615fcb29b8bca88b99653813d1b82c2
mosbaheliesse <eliesse91@hotmail.fr>
2025-06-26 12:34:58 +0200

    modif cards et ajout formulaire

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,29 +1,65 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
-import {MatCardModule} from  '@angular/material/card' ;
 import { STATUS, Todo } from '../../models/todo.interface';
 import { CommonModule } from '@angular/common';
-
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,MatCardModule, MatButtonModule, MatChipsModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatChipsModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
 export class HomeComponent {
-   todos:Todo[] = [
+
+  todos:Todo[] = [
     {
       id: 1,
-      title: "Poulpe",
-      content: 'Sa maman le poulpe',
-      deadline: new Date(),
-      status: STATUS.IN_PROGRESS,
+      title:"Finir tp todo",
+      content: 'vite vite',
+      deadline: new Date('2025/06/25 17:00:00'),
+      status: STATUS.BUG,
+    },
+        {
+      id: 1,
+      title:"Finir tp todo",
+      content: 'vite vite',
+      deadline: new Date('2025/06/25 17:00:00'),
+      status: STATUS.TODO,
+    },
+        {
+      id: 1,
+      title:"Finir tp todo",
+      content: 'vite vite',
+      deadline: new Date('2025/06/25 17:00:00'),
+      status: STATUS.TODO,
+    },
+        {
+      id: 1,
+      title:"Finir tp todo",
+      content: 'vite vite',
+      deadline: new Date('2025/06/25 17:00:00'),
+      status: STATUS.TODO,
+    },
+        {
+      id: 1,
+      title:"Finir tp todo",
+      content: 'vite vite',
+      deadline: new Date('2025/06/25 17:00:00'),
+      status: STATUS.TODO,
     }
-  ]
+  ];
+
+    statusOptions:{label:string,value:number}[] = Object.entries(STATUS)
+    .filter(([key, value]) => typeof value === 'number') // évite les doublons inversés
+    .map(([key, value]) => ({
+      label: key.replace('_', ' '), // remplacement des _ par un espace
+      value: value as number
+    }));
 
 }
-  
